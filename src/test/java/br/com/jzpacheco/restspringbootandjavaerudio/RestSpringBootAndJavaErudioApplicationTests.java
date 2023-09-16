8249a28bff1da382344c4d84fb84349cb73f5e74
jzpacheco <jeffersonpacheco@hotmail.com>
2023-09-16 20:41:22 -0300

    Restructing directory

---
+++ src/test/java/br/com/jzpacheco/restspringbootandjavaerudio/RestSpringBootAndJavaErudioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jzpacheco.restspringbootandjavaerudio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestSpringBootAndJavaErudioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
