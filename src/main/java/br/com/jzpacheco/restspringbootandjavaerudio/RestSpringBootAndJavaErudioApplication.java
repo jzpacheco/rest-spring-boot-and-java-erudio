ffee9779f7d4f215bb4b3b24144e6d3e92ce0b50
jzpacheco <jeffersonpacheco@hotmail.com>
2023-09-16 20:38:17 -0300

    Restructing directory

---
+++ src/main/java/br/com/jzpacheco/restspringbootandjavaerudio/RestSpringBootAndJavaErudioApplication.java
@@ -0,0 +1,13 @@
+package br.com.jzpacheco.restspringbootandjavaerudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestSpringBootAndJavaErudioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestSpringBootAndJavaErudioApplication.class, args);
+	}
+
+}
