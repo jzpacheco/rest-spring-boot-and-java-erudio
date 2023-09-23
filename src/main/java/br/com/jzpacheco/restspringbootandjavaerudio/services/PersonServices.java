09767fd6ba39f4f6fdb15f7683552820379260f9
jzpacheco <jeffersonpacheco@hotmail.com>
2023-09-23 14:20:34 -0300

    Migrations with Flyway

--- src/main/java/br/com/jzpacheco/restspringbootandjavaerudio/services/PersonServices.java
+++ src/main/java/br/com/jzpacheco/restspringbootandjavaerudio/services/PersonServices.java
@@ -8,9 +8,7 @@ import br.com.jzpacheco.restspringbootandjavaerudio.vo.v1.PersonVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
 @Service
---
+++ src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,7 @@
+INSERT INTO `person` (`id`,`address`,`first_name`,`gender`,`last_name`) VALUES (1,'São Paulo','Ayrton','Male','Senna');
+INSERT INTO `person` (`id`,`address`,`first_name`,`gender`,`last_name`) VALUES (3,'Porbandar - India','Indira','Female','Gandhi');
+INSERT INTO `person` (`id`,`address`,`first_name`,`gender`,`last_name`) VALUES (4,'Anchiano - Italy','Leonardo','Male','da Vinci');
+INSERT INTO `person` (`id`,`address`,`first_name`,`gender`,`last_name`) VALUES (5,'Porbandar - India','Mahatma','Male','Gandhi');
+INSERT INTO `person` (`id`,`address`,`first_name`,`gender`,`last_name`) VALUES (7,'Mvezo - South Africa','Nelson','Male','Mandela');
+INSERT INTO `person` (`id`,`address`,`first_name`,`gender`,`last_name`) VALUES (8,'Smiljan - Croatia','Nikola','Male','Tesla');
+INSERT INTO `person` (`id`,`address`,`first_name`,`gender`,`last_name`) VALUES (9,'Kentucky - US','Muhammad','Male','Ali');
