0e0e60658ff547542b8937501fd29fc8ffb91a7a
jzpacheco <jeffersonpacheco@hotmail.com>
2023-10-10 23:52:46 -0300

    Abstraction Class

--- src/test/java/br/com/jzpacheco/restspringbootandjavaerudio/integrationtests/testcontainers/AbstractionIntegrationTest.java
+++ src/test/java/br/com/jzpacheco/restspringbootandjavaerudio/integrationtests/testcontainers/AbstractionIntegrationTest.java
@@ -1,4 +1,13 @@
 package br.com.jzpacheco.restspringbootandjavaerudio.integrationtests.testcontainers;
 
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+
+@ContextConfiguration(inheritInitializers = AbstractionIntegrationTest.Initializer.class)
 public class AbstractionIntegrationTest {
+
+    public class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+
+    }
 }
